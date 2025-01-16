ea4c95296162c00b7365880caed7458889f02f0c
Luiz Kim <luiz.kim@controleonline.com>
2025-01-15 23:29:56 -0300

    Update

--- src/store/invoice/customActions.js
+++ src/store/invoice/customActions.js
@@ -59,7 +59,7 @@ export function getPaylist({ commit }, data) {
   };
 
   return api
-    .fetch('invoice-paylist', options)
+    .fetch('paylist', options)
 
     .then((data) => {
       commit(types.SET_ISLOADING, false);
